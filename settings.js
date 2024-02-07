cd51f62779609345e990297e340f32f86637c3ba
Белоусов Карл <141405429+karlbelousov@users.noreply.github.com>
2024-02-07 13:01:33 +0600

    страница результатов

--- settings.js
+++ settings.js
@@ -1,2 +1,10 @@
 // Файл для настроек приложения
 const TIMEOUT = 800;
+
+const AppRoute = {
+    Game: 'game',
+    Results: 'results',
+  };
+  
+  window.TIMEOUT = TIMEOUT;
+  window.AppRoute = AppRoute;
